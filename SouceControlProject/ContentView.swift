029911ea6d318afbd89779d8aacce4ee88688162
Dre <c0dingw.dre@gmail.com>
2023-08-17 21:30:44 -0700

    Added rectangle

--- SouceControlProject/ContentView.swift
+++ SouceControlProject/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hey, whats up!")
+            Rectangle().foregroundColor(.indigo)
         }
         .padding()
     }
