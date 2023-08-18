c2f5d5373bc7e0f10d6af9470ccac41f71c7533d
Dre <c0dingw.dre@gmail.com>
2023-08-17 20:26:25 -0700

    Initial Commit

---
+++ SouceControlProject/SouceControlProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SouceControlProjectApp.swift
+//  SouceControlProject
+//
+//  Created by Andres Gutierrez on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SouceControlProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
