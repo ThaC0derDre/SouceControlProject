0ef2dce0a26373d551cd5405c543b2e7913a4717
Dre <c0dingw.dre@gmail.com>
2023-08-17 20:44:21 -0700

    Updated ContentView to say 'what's up'

--- SouceControlProject/ContentView.swift
+++ SouceControlProject/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "hand.raised.fingers.spread.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hey, whats up!")
         }
         .padding()
     }
